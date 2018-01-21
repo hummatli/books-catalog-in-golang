32def87b571bf6f999a248ae9470ff5cdc2cf905
Sattar Hummatli <settarxan@gmail.com>
2018-01-21 13:06:36 +0400

    add templates and use request params in template

--- main.go
+++ main.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
+type Page struct {
+	Name string
+}
+
 func main() {
+	templates := template.Must(template.ParseFiles("templates/index.html"))
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello, Go Web Development")
+		p := Page{Name: "Gopher"}
+
+		if name := r.FormValue("name"); name != "" {
+			p.Name = name
+		}
+
+		if err := templates.ExecuteTemplate(w, "index.html", p); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 	})
 
 	fmt.Println(http.ListenAndServe(":8080", nil))
