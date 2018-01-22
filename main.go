256481908b98bd80f493df3ddec88b9c4b763f4c
Sattar Hummatli <settarxan@gmail.com>
2018-01-22 12:02:45 +0400

    add dump book list and get it from search url and show it in index.html via ajax

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"net/http"
@@ -12,6 +13,13 @@ import (
 type Page struct {
 	Name     string
 	DBStatus bool
+}
+
+type SearchResult struct {
+	Title  string
+	Author string
+	Year   string
+	ID     string
 }
 
 func main() {
@@ -28,6 +36,19 @@ func main() {
 		p.DBStatus = db.Ping() == nil
 
 		if err := templates.ExecuteTemplate(w, "index.html", p); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
+		results := []SearchResult{
+			SearchResult{"Moby-Dick", "Herman Melville", "1851", "222222"},
+			SearchResult{"The Adventures of Huckleberry Finn", "Mark Twain", "1884", "444444"},
+			SearchResult{"The Catcher in the Rye", "JD Salinger", "1951", "333333"},
+		}
+
+		encoder := json.NewEncoder(w)
+		if err := encoder.Encode(results); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	})
