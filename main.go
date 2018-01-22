cfb3d9835c2b51c14070f0e523ac27861a85c2f2
Sattar Hummatli <settarxan@gmail.com>
2018-01-22 13:03:02 +0400

    add functionality to ad selected books to local db

--- main.go
+++ main.go
@@ -1,88 +1,132 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
-	"net/http"
+  "fmt"
+  "net/http"
+  "html/template"
 
-	"database/sql"
+  "database/sql"
+  _ "github.com/mattn/go-sqlite3"
 
-	_ "github.com/mattn/go-sqlite3"
-
-	"encoding/json"
-	"encoding/xml"
-	"io/ioutil"
-	"net/url"
+  "encoding/json"
+  "net/url"
+  "io/ioutil"
+  "encoding/xml"
 )
 
 type Page struct {
-	Name     string
-	DBStatus bool
+  Name string
+  DBStatus bool
 }
 
 type SearchResult struct {
-	Title  string `xml:"title,attr"`
-	Author string `xml:"author,attr"`
-	Year   string `xml:"hyr,attr"`
-	ID     string `xml:"owi,attr"`
+  Title string `xml:"title,attr"`
+  Author string `xml:"author,attr"`
+  Year string `xml:"hyr,attr"`
+  ID string `xml:"owi,attr"`
 }
 
 func main() {
-	templates := template.Must(template.ParseFiles("templates/index.html"))
+  templates := template.Must(template.ParseFiles("templates/index.html"))
 
-	db, _ := sql.Open("sqlite3", "dev.db")
+  db, _ := sql.Open("sqlite3", "dev.db")
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		p := Page{Name: "Gopher"}
-		if name := r.FormValue("name"); name != "" {
-			p.Name = name
-		}
-		p.DBStatus = db.Ping() == nil
+  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    p := Page{Name: "Gopher"}
+    if name := r.FormValue("name"); name != "" {
+      p.Name = name
+    }
+    p.DBStatus = db.Ping() == nil
 
-		if err := templates.ExecuteTemplate(w, "index.html", p); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-	})
+    if err := templates.ExecuteTemplate(w, "index.html", p); err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+  })
 
-	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
-		var results []SearchResult
-		var err error
+  http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
+    var results []SearchResult
+    var err error
 
-		sValue := r.FormValue("search")
-		fmt.Println("sValue:", sValue)
-		if results, err = search(sValue); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
+    if results, err = search(r.FormValue("search")); err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
 
-		encoder := json.NewEncoder(w)
-		if err := encoder.Encode(results); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-	})
+    encoder := json.NewEncoder(w)
+    if err := encoder.Encode(results); err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+  })
 
-	fmt.Println(http.ListenAndServe(":8080", nil))
+  http.HandleFunc("/books/add", func (w http.ResponseWriter, r *http.Request) {
+    var book ClassifyBookResponse
+    var err error
+
+    if book, err = find(r.FormValue("id")); err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+    if err = db.Ping(); err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+
+    _, err = db.Exec("insert into books (pk, title, author, id, classification) values (?, ?, ?, ?, ?)",
+                      nil, book.BookData.Title, book.BookData.Author, book.BookData.ID, book.Classification.MostPopular)
+
+    if err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+  })
+
+  fmt.Println(http.ListenAndServe(":8080", nil))
 }
 
 type ClassifySearchResponse struct {
-	Results []SearchResult `xml:"works>work"`
+  Results []SearchResult `xml:"works>work"`
+}
+
+type ClassifyBookResponse struct {
+  BookData struct {
+    Title string `xml:"title,attr"`
+    Author string `xml:"author,attr"`
+    ID string `xml:"owi,attr"`
+  } `xml:"work"`
+  Classification struct {
+    MostPopular string `xml:"sfa,attr"`
+  } `xml:"recommendations>ddc>mostPopular"`
+}
+
+func find(id string) (ClassifyBookResponse, error) {
+  var c ClassifyBookResponse
+  body, err := classifyAPI("http://classify.oclc.org/classify2/Classify?summary=true&owi=" + url.QueryEscape(id))
+
+  if err != nil {
+    return ClassifyBookResponse{}, err
+  }
+
+  err = xml.Unmarshal(body, &c)
+  return c, err
 }
 
 func search(query string) ([]SearchResult, error) {
-	fmt.Println("-----------1----------- query:", query)
-	var resp *http.Response
-	var err error
+  var c ClassifySearchResponse
+  body, err := classifyAPI("http://classify.oclc.org/classify2/Classify?summary=true&title=" + url.QueryEscape(query))
 
-	if resp, err = http.Get("http://classify.oclc.org/classify2/Classify?&summary=true&title=" + url.QueryEscape(query)); err != nil {
-		return []SearchResult{}, err
-	}
+  if err != nil {
+    return []SearchResult{}, err
+  }
 
-	defer resp.Body.Close()
-	var body []byte
-	if body, err = ioutil.ReadAll(resp.Body); err != nil {
-		return []SearchResult{}, err
-	}
+  err = xml.Unmarshal(body, &c)
+  return c.Results, err
+}
 
-	var c ClassifySearchResponse
-	err = xml.Unmarshal(body, &c)
-	return c.Results, err
+func classifyAPI(url string) ([]byte, error) {
+  var resp *http.Response
+  var err error
+
+  if resp, err = http.Get(url); err != nil {
+    return []byte{}, err
+  }
+
+  defer resp.Body.Close()
+
+  return ioutil.ReadAll(resp.Body)
 }
